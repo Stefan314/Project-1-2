6088aee8368001d962d96f700edb04171e6dee41
Stefan314 <stefan.1999.deelen@gmail.com>
2020-03-16 13:14:04 +0100

    simple getter methods

--- core/src/com/crazyputting/objects/PuttingCourse.java
+++ core/src/com/crazyputting/objects/PuttingCourse.java
@@ -2,29 +2,35 @@ package com.crazyputting.objects;
 
 import com.badlogic.gdx.math.Vector3;
 import com.crazyputting.function.Function;
+import jdk.nashorn.internal.ir.FunctionCall;
 
 public class PuttingCourse {
+
+    private Vector3 flag;
+    private Vector3 start;
+    private Function height;
+    private double frictionCoefficient;
+    final private double maximumVelocity = 3.0;
+
     public PuttingCourse(Function height,
-                         Vector3 flag, Vector3 start){
-
+                         Vector3 flag, Vector3 start, double mu){
+        this.flag = flag;
+        this.start = start;
+        this.height = height;
+        this.frictionCoefficient = mu;
     }
-    public void get_height(){
 
-    }
-    public void get_flag_position(){
+    public double get_height(Vector3 pos){ return height.evaluate(pos); }
 
-    }
-    public void get_start_position(){
+    public Vector3 get_flag_position(){ return flag; }
 
-    }
-    public void get_friction_coefficient(){
+    public Vector3 get_start_position(){ return start; }
 
-    }
-    public void get_maximum_velocity(){
+    public double get_friction_coefficient(){ return frictionCoefficient; }
 
-    }
+    public double get_maximum_velocity(){ return maximumVelocity; }
+
     public void get_hole_tolerance(){
 
     }
 }
-
